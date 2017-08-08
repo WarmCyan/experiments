205c2faff290de7071e98af7c4391dc8cef84219
Nathan Martindale <nmblenderdude0@gmail.com>
2017-08-08 17:34:52 -0500

    Started work on LVNLA dataset generator

---
+++ 1_LVNLA/datagen.py
@@ -0,0 +1,22 @@
+import csv
+
+# NOTE: domain format: [min,max] (array)
+# NOTE: size refers to number of bits
+
+def gen_basic_addition(count, i1_domain, i2_domain):
+    pass
+
+def gen_basic_exp(count, i1_domain, i2_domain):
+    pass
+
+def gen_binary_addition(count, i1_size, i2_size, output_size):
+    pass
+
+def gen_binary_exp(count, i1_size, i2_size, output_size):
+    pass
+
+def gen_circle_class(count, i1_domain, i2_domain, h_domain, k_domain, r_domain):
+    pass
+
+def gen_quadratic_class(count, a_domain, b_domain, c_domain):
+    pass
