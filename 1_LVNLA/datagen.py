0aee2d971925859781ed01371db2159b7bd9da41
Nathan Martindale <nmblenderdude0@gmail.com>
2017-08-08 18:52:01 -0500

    Finished two datageneration functions for LVNLA

--- 1_LVNLA/datagen.py
+++ 1_LVNLA/datagen.py
@@ -1,13 +1,56 @@
 import csv
+import random as r
 
 # NOTE: domain format: [min,max] (array)
 # NOTE: size refers to number of bits
 
+def rand(domain):
+    return r.randint(domain[0], domain[1])
+
+def randbinary(size):
+    
+
+def save(filename, data):
+    print("Saving data to '" + str(filename) + "'...")
+    with open(filename, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+
+        for entry in data:
+            writer.writerow(entry)
+    print("Saved!")
+        
+
 def gen_basic_addition(count, i1_domain, i2_domain):
-    pass
+    print("Generating basic addition dataset of size " + str(count) + "...")
+    print("Domains: " + str(i1_domain) + "," + str(i2_domain))
+    
+    data = []
+    
+    for i in range(0,count):
+        i1 = rand(i1_domain)
+        i2 = rand(i2_domain)
+        output = i1+i2
+
+        data.append([i1, i2, output])
+
+    print("Generated!")
+    return data
 
 def gen_basic_exp(count, i1_domain, i2_domain):
-    pass
+    print("Generating basic addition dataset of size " + str(count) + "...")
+    print("Domains: " + str(i1_domain) + "," + str(i2_domain))
+    
+    data = []
+    
+    for i in range(0,count):
+        i1 = rand(i1_domain)
+        i2 = rand(i2_domain)
+        output = i1**i2
+
+        data.append([i1, i2, output])
+
+    print("Generated!")
+    return data
 
 def gen_binary_addition(count, i1_size, i2_size, output_size):
     pass
---
+++ 1_LVNLA/driver.py
@@ -0,0 +1,6 @@
+from datagen import *
+
+result = gen_basic_addition(50, [0,10], [0,10])
+save("test.csv", result)
+#result = gen_basic_exp(10, [0,10], [0,10])
+#print(result)
