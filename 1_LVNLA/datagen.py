c0fe5788c3343118e19efe5385a11f8352569f47
Nathan Martindale <nmblenderdude0@gmail.com>
2017-08-09 17:41:36 -0500

    Completed some more data generation functions

--- 1_LVNLA/datagen.py
+++ 1_LVNLA/datagen.py
@@ -1,4 +1,5 @@
 import csv
+import math
 import random as r
 
 # NOTE: domain format: [min,max] (array)
@@ -8,7 +9,7 @@ def rand(domain):
     return r.randint(domain[0], domain[1])
 
 def randbinary(size):
-    
+    pass
 
 def save(filename, data):
     print("Saving data to '" + str(filename) + "'...")
@@ -59,7 +60,54 @@ def gen_binary_exp(count, i1_size, i2_size, output_size):
     pass
 
 def gen_circle_class(count, i1_domain, i2_domain, h_domain, k_domain, r_domain):
-    pass
+    print("Generating quadratic classification dataset of size " + str(count) + "...")
+    print("Domains: " + str(i1_domain) + "," + str(i2_domain) + "," + str(h_domain) + "," + str(k_domain) + "," + str(r_domain))
+    
+    data = []
 
-def gen_quadratic_class(count, a_domain, b_domain, c_domain):
-    pass
+    for i in range(0, count):
+        i1 = rand(i1_domain)
+        i2 = rand(i2_domain)
+        h = rand(h_domain)
+        k = rand(k_domain)
+        r = rand(r_domain)
+        
+        point = (i1-h)**2 + (i2-k)**2
+        output = 0
+        print(str([i1,i2,h,k,r]))
+        print(str(point) + " " + str(r**2))
+        if point <= r**2:
+            output = 1
+        
+        data.append([i1,i2,h,k,r,output])
+        
+    print("Generated!")
+    return data
+
+# NOTE: for now, only going to do non-imaginary
+def gen_quadratic(count, a_domain, b_domain, c_domain):
+    print("Generating quadratic dataset of size " + str(count) + "...")
+    print("Domains: " + str(a_domain) + "," + str(b_domain) + "," + str(c_domain))
+
+    data = []
+
+    for i in range(0, count):
+        a = 0
+        b = 0
+        c = 0
+
+        desc = -1
+        while desc < 0:
+            a = rand(a_domain)
+            b = rand(b_domain)
+            c = rand(b_domain)
+            desc = b**2 - 4*a*c
+            if a == 0: desc = -1
+        
+        output1 = (-b - math.sqrt(desc))/(2*a)
+        output2 = (-b + math.sqrt(desc))/(2*a)
+
+        data.append([a,b,c,output1,output2])
+    
+    print("Generated!")
+    return data
--- 1_LVNLA/driver.py
+++ 1_LVNLA/driver.py
@@ -1,6 +1,9 @@
 from datagen import *
 
-result = gen_basic_addition(50, [0,10], [0,10])
-save("test.csv", result)
+#result = gen_basic_addition(50, [0,10], [0,10])
+##save("test.csv", result)
 #result = gen_basic_exp(10, [0,10], [0,10])
 #print(result)
+#result = gen_quadratic(10, [0,10],[0,10],[0,10])
+result = gen_circle_class(10, [1,10],[1,10],[1,10],[1,10], [1,3])
+print(result)
